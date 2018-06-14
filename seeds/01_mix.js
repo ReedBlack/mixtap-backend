afa2d253a6faef393ced3226325b9c22b2b535e8
REED BLACK <reedblack@REEDs-MacBook-Air.local>
2018-06-14 16:00:22 -0600

    changing server datatypes to reflect client-side

--- seeds/01_mix.js
+++ seeds/01_mix.js
@@ -7,21 +7,21 @@ exports.seed = function (knex, Promise) {
           id: 1,
           DJ: "Reed Black",
           Title: "September 2017 Yoga Mix",
-          File: "none",
+          selectedFile: null,
           Tags: "chill meditation lounge yoga"
         },
         {
           id: 2,
           DJ: "Vince Staples",
           Title: "Yeah Right",
-          File: "none",
+          selectedFile: null,
           Tags: "rap bass hip-hop"
         },
         {
           id: 3,
           DJ: "Bob Moses",
           Title: "BBC Essential Mix 2016",
-          File: "none",
+          selectedFile: null,
           Tags: "chill house lounge experimental"
         }
       ])
