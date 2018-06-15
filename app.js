a93273e72ca70f782d925003a57919491447c91c
REED BLACK <reedblack@REEDs-MacBook-Air.local>
2018-06-15 15:34:15 -0600

    routes

--- app.js
+++ app.js
@@ -10,18 +10,8 @@ const aws = require("aws-sdk");
 const multer = require("multer");
 const queries = require("./queries");
 
-// app.use(morgan('dev'));
+app.use(morgan('dev'));
 
-if (app.get('env') == 'production') {
-    app.use(morgan('common', {
-        skip: function (req, res) {
-            return res.statusCode < 400
-        },
-        stream: __dirname + '/../morgan.log'
-    }));
-} else {
-    app.use(morgan('dev'));
-}
 
 app.use(bodyParser.json());
 app.use(cors())
@@ -55,7 +45,6 @@ app.get("/upload", (request, response, next) => {
 app.post("/upload",
 
     upload.array("audio", 1), (request, response) => {
-        console.log(request)
         response.json({
             audioUrl: `${request.files[0].location}`
         });
